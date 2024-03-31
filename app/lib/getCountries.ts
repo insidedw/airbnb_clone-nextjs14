2e5fbce90bcca1b7e717960667a1552a5b6ff047
insidedw <insidedw@gmail.com>
2024-03-31 16:22:20 +0900

    order country name

--- app/lib/getCountries.ts
+++ app/lib/getCountries.ts
@@ -1,12 +1,16 @@
 import countries from 'world-countries'
 
-const countriesFormatted = countries.map((country) => ({
-  value: country.cca2,
-  label: country.name.common,
-  flag: country.flag,
-  latLng: country.latlng,
-  region: country.region,
-}))
+const countriesFormatted = countries
+  .map((country) => ({
+    value: country.cca2,
+    label: country.name.common,
+    flag: country.flag,
+    latLng: country.latlng,
+    region: country.region,
+  }))
+  .sort((a, b) => {
+    return a.label.toUpperCase() < b.label.toUpperCase() ? -1 : a.label > b.label ? 1 : 0
+  })
 
 export const useCountries = () => {
   const getAllCountries = () => countriesFormatted
